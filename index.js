38d6e94360498030d703c4d30ce91ffb650078ac
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-06 13:23:04 -0700

    Practiced with Javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+// PARAMETERS AND ARGUMENTS - the way I remember is P.A.
+
+// KEYWORDS ARE THINGS AUTOMATICALLY RECOGNIZED BY THE CODE AT RUNTIME
+
+// var something = "Hello, World";
+
+// console.log(something)
+
+// function cookies(){
+//     console.log("Hello World")
+//     console.log(12345)
+// }
+
+// cookies();
+
+var args = "hello bananas"
+var pizza = "mama mia"
+var nums = 12345
+
+function bananas(params){
+    console.log(params)
+}
+
+bananas(args);
+bananas(pizza);
+bananas(nums)
